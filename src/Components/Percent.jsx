00a25a50d9e002171cc9fdf458dc642ed807e8b3
farhadjony <farhadjony.math@gmail.com>
2024-12-03 09:26:18 +0600

    Percent Top part done

--- src/Components/Percent.jsx
+++ src/Components/Percent.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Percent() {
   return (
     <section className=''>
-        <div className='grid drid-cols-2  w-full bg-[url(Percent.png)] bg-no-repeat bg-cover p-16 pl-24 space-y-4'>
-            <h1 className='font-swmibold text-[#FFFFFF] w-1/3 text-xl md:text-2xl lg:text-3xl'>Allocate effort where your reps make an inpact.</h1>
-            <h2 className='italic font-semibold text-[#0FF1F6] text-xl md:text-2xl lg:text-3xl'>Let us handle the rest.</h2>
-            <p className='text-[#FFFFFF]/60 w-1/3 text-sm md:text-[14px]'>Keep your reps “in the air” -- out in the field and on the phone where they can build relationships.</p>
+        <div className='grid grid-cols-1 w-full bg-[url(Percent.png)] bg-no-repeat bg-cover p-16 pl-16 md:pl-24  space-y-4'>
+            <h1 className='font-semibold text-[#FFFFFF] w-1/2 text-lg md:text-2xl lg:text-4xl'>Allocate effort where your reps make an inpact.</h1>
+            <h2 className='italic font-semibold text-[#0FF1F6] text-lg md:text-2xl lg:text-4xl'>Let us handle the rest.</h2>
+            <p className='text-[#FFFFFF]/60 w-1/2 text-medium md:text-[20px]'>Keep your reps “in the air” -- out in the field and on the phone where they can build relationships.</p>
             
         </div>
       
